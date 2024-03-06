cf027e9663fcdd4996b9d154f2e8ac3f1eae8e8c
josemiguel-AAA <ocean15aguasol@gmail.com>
2024-03-06 10:13:44 -0500

    main

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:appsol_final/components/login.dart';
+import 'package:appsol_final/provider/pedido_provider.dart';
+import 'package:appsol_final/provider/ubicacion_provider.dart';
+import 'package:appsol_final/provider/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:camera/camera.dart';
+// Importa el paquete permission_handler
+
+late List<CameraDescription> camera;
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  //camera = await availableCameras();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => UserProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => PedidoProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => UbicacionProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        //title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Login(),
+        /*home: BarraNavegacion(
+          indice: 0,
+          subIndice: 0,
+        ),*/
+      ),
+    );
+  }
+}
